050a2306cc26ceec0e6a9f66976287f897cf0091
chestnut55 <404852166@qq.com>
2018-03-10 16:25:18 +0800

    add test dataset

--- src/data_load.py
+++ src/data_load.py
@@ -24,7 +24,7 @@ def obesity_data():
     # normalize make the elm work better
     f = (f - f.min()) / (f.max() - f.min())
 
-    return train_test_split(f, one_hot_encoded, test_size=0.1, random_state=42)  # Can change to 0.2
+    return train_test_split(f, one_hot_encoded, test_size=0.2, random_state=42)  # Can change to 0.2
 
 def obesity_gene_marker_data():
     marker_presence = 'marker_presence_obesity.txt'
@@ -49,3 +49,34 @@ def obesity_gene_marker_data():
 
     return train_test_split(
         f, one_hot_encoded, test_size=0.2, random_state=42)  # Can change to 0.2
+
+
+def hmp_hmpii_data():
+    abundance = 'abundance_hmp-hmpii-ibd-cirrhosis-t2d_long-t2d_short-WT2D-colorectal--bodysite-stool--disease-n-t2d.txt'
+    f = pd.read_csv(abundance, sep='\t', header=None, index_col=0, dtype=unicode)
+    f = f.T
+    f.set_index('sampleID', inplace=True)
+
+    l = f['disease']
+    l = l.replace(to_replace=['small_adenoma', 'large_adenoma'], value=['n', 'n'])
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(l)
+
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+
+    one_hot_encoder = OneHotEncoder(sparse=False)
+    one_hot_encoded = one_hot_encoder.fit_transform(integer_encoded)
+
+    feature_identifier = 'k__'
+    feat = [s for s in f.columns if sum([s2 in s for s2 in feature_identifier.split(':')]) > 0]
+    f = f.loc[:, feat].astype('float')
+    # normalize make the elm work better
+    # f = (f - f.min()) / (f.max() - f.min())
+
+    return train_test_split(f, one_hot_encoded, test_size=0.1, random_state=42)  # Can change to 0.2
+
+
+if __name__ == '__main__':
+    col1 = hmp_hmpii_data()
+    col2 = obesity_data()
+    print len(col2-col1)
